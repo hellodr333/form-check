1440163e9779c23089be641cc832dbeca6efcd89
chenwewe <chenwewe@outlook.com>
2016-12-10 17:31:46 +0800

    log

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
 (function(){
+	
+
 	var myapp=angular.module('myapp',['ngMessages']);
-	myapp.controller('myctrl',function($scope){
+	myapp.controller('formCtrl',function($scope,$http){
 		// 用户数据
 		$scope.user={};
 		// 复选框
@@ -11,12 +13,37 @@
 		};
 		$scope.submitForm = function(json){
 			$scope.user = json;
+			console.log(json)
+			//$http.get('url',{params:$scope.user}).success(function(){}).error(function(){})
+			
 		};
-		
-		$scope.check = function(){
-			console.log($scope.myform)
+
+	});
+
+	var data=[{isFinish:true,name:'html'},{isFinish:true,name:'css'}];
+	myapp.controller('tabCtrl',function($scope){
+		$scope.tab=0;
+		$scope.items = data;
+		$scope.aa={};
+
+		$scope.setTab=function(which){
+			$scope.tab=which;
+		};
+		$scope.isSet=function(which){
+			return $scope.tab===which;
+		};
+
+		$scope.addItem=function(json){
+			$scope.items.push({
+				isFinish:json.state,name:json.name
+			})
+			json.state=true;
+			json.name='';
 		}
 	});
+
+
+
 
 
 
